7a6ed75928ee8e25088b2fb347c88bad3175061f
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-06 18:38:48 +0500

    file05

--- file05.py
+++ file05.py
@@ -6,5 +6,18 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    c = 0
+    c_n = 0
+    l = [c,c_n]
+    for i in data:
+        if i.isdigit():
+            c+=1
+        elif not(i.isdigit()):
+            c_n += 1
+        l = [c,c_n]
+    return l
     
 # Read data from file
+file = open('txt_file/data05.txt','r')
+data = file.read()
+print(main(data))
