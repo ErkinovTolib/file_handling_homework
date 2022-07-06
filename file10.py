d5403b07e70d92c8b70112fd3724f6a9ef377a3b
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-06 22:29:50 +0500

    file10

--- file10.py
+++ file10.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    arr_1 = []
+    arr = data.split('\n')
+    for i in arr:
+        arr_1.append(len(i))
+    return arr_1
 
 # Read data from file
+f = open('txt_file/data10.txt','r')
+data = f.read()
+print(main(data))
