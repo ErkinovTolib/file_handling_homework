c41b454ebe9bd15e9c185adc59530f8219ddaca6
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-06 18:39:47 +0500

    file09

--- file09.py
+++ file09.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    ans = []
+    for i in data:
+        if i.isdigit():
+            ans.append(i)
+    return min(ans)
 
 # Read data from file
+f = open('txt_file/data09.txt','r')
+data = f.read()
+print(main(data))
