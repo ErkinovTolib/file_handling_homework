e300ebbdb6364c8bf26a560ce764b9bc720f9a2a
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-06 18:13:26 +0500

    file03

--- file03.py
+++ file03.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    ans = []
+    for i in data:
+        if i.isdigit():
+            ans.append(i)
+    return ans
+        
     
 # Read data from file
+f = open('txt_file/data03.txt')
+data = f.read()
+print(main(data))
