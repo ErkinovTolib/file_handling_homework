2700124056ee700fffd6d541bb7604e8e1e559ca
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-06 18:38:27 +0500

    file04

--- file04.py
+++ file04.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    ans = []
+    for i in data:
+        if i.isalpha():
+            ans.append(i)
+    return ans
     
 # Read data from file
+f = open('txt_file/data04.txt','r')
+data = f.read()
+print(main(data))
