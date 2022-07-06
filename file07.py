12b36e82f718d245154743ca444a9fc358545665
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-06 18:04:03 +0500

    file07

--- file07.py
+++ file07.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    for i in data:
+        l = len(data)
+    ans = [l]
     
+    return ans 
 # Read data from file
+f = open('txt_file/data06.txt')
+data = f.read()
+print(main(data))
